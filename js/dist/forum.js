14d8da64a38bfe9f8a3f0ebf2817983b55d90a5a
sintos <ysintos@gmail.com>
2026-07-04 01:14:17 +0300

    Register frontend bundle under both Flarum IDs

--- js/dist/forum.js
+++ js/dist/forum.js
@@ -5,24 +5,27 @@
   var Button = flarum.core.compat['common/components/Button'];
   var LogInButtons = flarum.core.compat['forum/components/LogInButtons'];
 
-  flarum.extensions['uoi-cas-sso'] = {
+  var extension = {
     extend: function (app) {
-    extend(LogInButtons.prototype, 'items', function (items) {
-      items.add(
-        'uoi-cas',
-        Button.component(
-          {
-            className: 'Button LogInButton LogInButton--uoi-cas',
-            icon: 'fas fa-university',
-            onclick: function () {
-              var returnUrl = encodeURIComponent(window.location.href);
-              window.location.href = app.forum.attribute('baseUrl') + '/auth/cas?return=' + returnUrl;
+      extend(LogInButtons.prototype, 'items', function (items) {
+        items.add(
+          'uoi-cas',
+          Button.component(
+            {
+              className: 'Button LogInButton LogInButton--uoi-cas',
+              icon: 'fas fa-university',
+              onclick: function () {
+                var returnUrl = encodeURIComponent(window.location.href);
+                window.location.href = app.forum.attribute('baseUrl') + '/auth/cas?return=' + returnUrl;
+              },
             },
-          },
-          app.translator.trans('uoi-cas-sso.forum.log_in.with_cas_button')
-        )
-      );
-    });
+            app.translator.trans('uoi-cas-sso.forum.log_in.with_cas_button')
+          )
+        );
+      });
     },
   };
+
+  flarum.extensions['uoi-cas-sso'] = extension;
+  flarum.extensions['uoi-flarum-cas-sso'] = extension;
 })();
